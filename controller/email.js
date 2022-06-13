8cdc16f9da50785f54171f7b8a7528b58f275228
sanjay <ksanjayk@protonmail.com>
2022-06-13 11:34:14 +0530

    basic email sender

--- controller/email.js
+++ controller/email.js
@@ -1,6 +1,25 @@
-const sendEmail = async(req,res)=>{
+const nodemailer = require("nodemailer");
 
-    res.send('email sent')
-}
+const sendEmail = async (req, res) => {
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host:process.env.MAIL_HOST,
+    port: 25,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+  let info = await transporter.sendMail({
+    from: '" Sanji " <sanji.blockchain@gmail.com>', // sender address
+    to: "ksanjay99kumar@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    html: "<b>Sending email with nodejs?</b>", // html body
+  });
 
-module.exports =sendEmail
+  console.log(info);
+  res.send(info);
+};
+
+
+module.exports = sendEmail;
